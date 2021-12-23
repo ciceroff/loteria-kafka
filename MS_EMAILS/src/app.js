49318498702894e18439362037ce094b9c31a550
ciceroff <cicero.barrozo13@gmail.com>
2021-12-23 17:20:15 -0300

    feat: configure kafka

--- MS_EMAILS/src/app.js
+++ MS_EMAILS/src/app.js
@@ -3,4 +3,27 @@ import { Kafka } from 'kafkajs';
 
 const app = express();
 
+const kafka = new Kafka({
+  brokers: ['localhost:9092'],
+});
+
+const producer = kafka.producer();
+
+await producer.connect();
+await producer.send({
+  topic: 'test-topic',
+  messages: [{ value: 'Hello KafkaJS user!' }],
+});
+await producer.disconnect();
+
+const consumer = kafka.consumer({ groupId: 'test-group' });
+await consumer.connect();
+await consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
+await consumer.run({
+  eachMessage: async ({ topic, partition, message }) => {
+    console.log({
+      value: message.value.toString(),
+    });
+  },
+});
 app.listen(3333);
